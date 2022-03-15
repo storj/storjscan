a68b1fa705c42b59bad6647394a2457fc0cc7dc7
Yaroslav Vorobiov <yar.vorobiov@gmail.com>
2022-03-15 18:39:24 +0100

    remove contract bytecode as we excpect to work with already deployed contract

--- tokens/gen.go
+++ tokens/gen.go
@@ -4,4 +4,4 @@
 package tokens
 
 //go:generate mkdir -p "erc20"
-//go:generate abigen --bin=../contracts/build/ERC20.bin --abi=../contracts/build/ERC20.abi --type=ERC20 --pkg=erc20 --out=erc20/erc20.go
+//go:generate abigen --abi=../contracts/build/ERC20.abi --type=ERC20 --pkg=erc20 --out=erc20/erc20.go
